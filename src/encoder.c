86a36f583e3089efa39a3eeb285a1a9f71068dd9
AryanDhabale <aryan.dhabale12@gmail.com>
2025-02-19 20:04:57 -0500

    gets raw data from image

--- src/encoder.c
+++ src/encoder.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
-
+#define STB_IMAGE_IMPLEMENTATION
+#include "./libraries/stb_image.h"
 
 int main() {
+
     FILE *file = fopen("./images/image.png", "rb");  // Open PNG file in binary mode
     FILE *output = fopen("./.txt/output.txt", "w");  // Open output file in write mode
+
+    
 
     if (file == NULL) {
         printf("Failed to open image file\n");
@@ -16,16 +20,22 @@ int main() {
         return 1;
     }
     
-    unsigned char buffer[5];
+
+   
+    
+    unsigned char buffer[30107];
     size_t bytesRead = fread(buffer, 1, sizeof(buffer), file);
     
     // Print raw binary data in hexadecimal format
     for (size_t i = 0; i < bytesRead; i++) {
         fprintf(output, "%02X ", buffer[i]);  // Print each byte in hex
+        if (i % 30 == 0) {
+            fprintf(output, "\n");
+        }
     }
-
+    
+    //fprintf(output, "width: %d\nheight: %d", width, height);
     fclose(output);
     fclose(file);
-
     return 0;
 }
