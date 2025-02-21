db0c83c62b550a3cab19f269d3650a7df13acfca
AryanDhabale <aryan.dhabale12@gmail.com>
2025-02-21 05:33:23 -0500

    decoding works

--- src/encoder.c
+++ src/encoder.c
@@ -90,6 +90,72 @@ void encode (char* imageData, char* message, char* key, int imageDataLength) {
     }
 }
 
+char* decode(char *binaryStr, char *key) {
+    size_t len = strlen(binaryStr);
+    char *keyInBinary = stringToBinary(key);
+    int keyStreak = 0;
+    int keyLength = strlen(key);
+    size_t numBytes = len / 8;
+    size_t extractedLength = 0;
+    
+    
+    // Allocate space for the extracted bits (as a string)
+    char *hiddenBits = malloc(numBytes + 1);
+    if (!hiddenBits) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        return NULL;
+    }
+    
+    // Extract the 8th bit (index 7) from each 8-character group.
+    for (size_t i = 0; i < numBytes; i++) {
+        // The LSB of the i-th byte is at position (i * 8 + 7)
+        hiddenBits[i] = binaryStr[i * 8 + 7];
+        extractedLength++;
+        if (keyInBinary[keyStreak] == binaryStr[i * 8 + 7]) {
+            keyStreak++;
+        } else if (binaryStr[i * 8 + 7] == 48) {
+            keyStreak = 1;
+        } else {
+            keyStreak = 0;
+        }
+        printf(" %d", keyStreak);
+        if (keyStreak == keyLength*8) {
+            break;
+            printf("loop is not broken out of");
+        }
+    }
+
+    printf("\nextracted length: %d", extractedLength);
+    hiddenBits[extractedLength] = '\0';
+
+    printf("\n");
+    for (int i = 0; i < strlen(hiddenBits); i++) {
+        printf("%c", hiddenBits[i]);
+    }
+    
+    // Now, every group of 8 bits in hiddenBits will form one ASCII character.
+    size_t messageLength = (extractedLength / 8) - strlen(key);
+    char *message = malloc(messageLength+1);
+    if (!message) {
+        fprintf(stderr, "Memory allocation failed.\n");
+        free(hiddenBits);
+        return NULL;
+    }
+    printf("\n message length: %zu" , messageLength);
+    for (size_t i = 0; i < messageLength; i++) {
+        int asciiVal = 0;
+        // Process 8 bits for one character.
+        for (int j = 0; j < 8; j++) {
+            asciiVal = (asciiVal << 1) | (hiddenBits[i * 8 + j] - '0');
+        }
+        message[i] = (char)asciiVal;
+    }
+    message[messageLength] = '\0';
+    
+    free(hiddenBits);
+    return message;
+}
+
 
 
 int main() {
@@ -99,8 +165,8 @@ int main() {
     //const char *outputFilePoint = "./output/outputImage.png";
     int fileSize;
     int width, height, channels;
-    const char KEY[] = "hi";
-    //const char MESSAGE[] = "Gooo Dawgs! Sic 'em! Woof wOof wOOf woOf!";
+    const char KEY[] = "AB";
+    const char MESSAGE[] = "dude";
     
     unsigned char *inputFilePoint = stbi_load("./images/image.png", &width, &height, &channels, 0);
     if (outputFile == NULL) {
@@ -113,15 +179,10 @@ int main() {
     fileSize = ftell(inputFile);
     printf("filesize: %d\n", fileSize);
     fseek(inputFile, 0, SEEK_SET);
-    
-
-
-    
-
 
     char *imageData = imageToBinary(inputFilePoint, width, height, channels);
     char *keyBinary = stringToBinary(KEY);
-    char *messageBinary = stringToBinary(KEY);
+    char *messageBinary = stringToBinary(MESSAGE);
     encode(imageData, messageBinary, keyBinary, width*height*channels);
 
     fprintf(outputFile, imageData, "%s");
@@ -130,13 +191,15 @@ int main() {
     //unsigned char* actualImageData = binaryToImage(imageData, width, height, channels);
     unsigned char* actualImageData = binaryToImage(imageData, width, height, channels);
     
-    printf("something happens");
+
     if (stbi_write_png("./output/outputImage.png", width, height, channels, actualImageData, width * channels)) {
         printf("Image successfully written to outputImage.png\n");
     } else {
         printf("Failed to write image\n");
     }
-        
+    
+
+    printf("\ndecoded message: %s", decode(imageData, KEY));
     // Close the output file and free the image data from memory
     fclose(outputFile);
     stbi_image_free(inputFilePoint);
